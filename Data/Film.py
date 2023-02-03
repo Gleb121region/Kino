db07b6534afd7c9e0f478173611c15f1987487f3
Gleb121region <71015316+Gleb121region@users.noreply.github.com>
2023-02-03 19:38:38 +0300

    rewrote the class Film.py now it uses a regular expression without replacement

--- Data/Film.py
+++ Data/Film.py
@@ -1,3 +1,5 @@
+import re
+
 from Data import Description
 
 
@@ -7,17 +9,24 @@ class Film(object):
         self.film_name: str = film_name
         self.film_id: int = film_id
         self.description = description
-    # todo: переписать без replace
+
     def send_info_about_film(self) -> dict:
-        text = (f'<b>{self.film_name}</b>\n'
-                f'Постер: {self.description.poster}\n'
-                f'Год производства: {self.description.year}\n'
-                f'Длительность: {self.description.length}\n'
-                f'Страна: {self.description.country}\n'
-                f'Жанр: {self.description.genre}\n'
-                f'Рейтинг по отзывам: {self.description.rating}'.replace("\'", '')
-                .replace(')', '').replace('(', '')
-                .replace(',', '').replace('[', '').replace(']', ''))
+        movie_title = re.search(r'[\w\s-]+', str(self.film_name)).group(0)
+        movie_poster = re.search(r'[\w://.]+', str(self.description.poster)).group(0)
+        movie_year = re.search(r'\d+', str(self.description.year)).group(0)
+        movie_length = re.search(r'\d+', str(self.description.length)).group(0)
+        movie_countries = re.search(r'[\w\s]+', str(self.description.country)).group(0)
+        movie_genres = re.search(r'[\w\s]+', str(self.description.genre)).group(0)
+        movie_rating = re.search(r'\d+\.\d+', str(self.description.rating)).group(0)
+
+        text = (f'<b>{movie_title}</b>\n'
+                f'Постер: {movie_poster}\n'
+                f'Год производства: {movie_year}\n'
+                f'Длительность: {movie_length}\n'
+                f'Страна: {movie_countries}\n'
+                f'Жанр: {movie_genres}\n'
+                f'Рейтинг по отзывам: {movie_rating}')
+
         from VX import VX
         movie_video_url = VX().get_film_link_by_kinopoisk_id(int(self.film_id))
         my_Dict = {movie_video_url: text}
